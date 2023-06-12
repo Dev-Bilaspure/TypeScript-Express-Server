2dafe1e94ed3a466fae49858954b878f18a6d2c2
Dev Bilaspure <devbilaspure4941@gmail.com>
2023-06-12 16:09:17 +0530

    bugs fixed

--- dist/index.js
+++ dist/index.js
@@ -5,9 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-app.get('/', (req, res) => {
-    res.json("This is express with typesccript!!!!!!!!!!!!!");
+const PORT = process.env.PORT || 8000;
+app.get("/", (req, res) => {
+    res.json("Your express app with typescript!!");
 });
-app.listen(8000, () => {
-    console.log('Listening to port 8000');
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}...`);
 });
