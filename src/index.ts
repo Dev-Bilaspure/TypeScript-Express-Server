c190d4fe79d313efa4e46a6312d4f253b002e569
Dev Bilaspure <devbilaspure4941@gmail.com>
2023-06-12 16:23:08 +0530

    bug fixed

--- src/index.ts
+++ src/index.ts
@@ -9,5 +9,5 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}...`);
+  console.log(`Server is running on port ${PORT}... \nStart using: http://localhost:8000/`);
 });
